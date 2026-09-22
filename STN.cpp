58d9d4f0bccaa9abff57d589c97e9bd355cc48db
pankajnage07-del <pankajnage07@gmail.com>
2026-09-22 19:16:10 +0530

    Add files via upload

---
+++ STN.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+    a=55;
+    cout << "Enter second number: ";
+    cin >> b;
+    b=40;
+    cout << "Subtraction = " << a - b;
+
+    return 0;
+}
