5f848fb559d9d5fd1efa6143f8ea719bdcfb5ca5
pankajnage07-del <pankajnage07@gmail.com>
2026-09-22 19:10:44 +0530

    Add files via upload

---
+++ ATN.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+    a=10;
+    cout << "Enter second number: ";
+    cin >> b;
+    b=20;
+    cout << "Addition = " << a + b;
+    return 0;
+}
