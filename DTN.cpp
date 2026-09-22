c2f82ffef4e1c2cde5b8ad06d2df4db1555e4028
pankajnage07-del <pankajnage07@gmail.com>
2026-09-22 19:26:24 +0530

    Add files via upload

---
+++ DTN.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Division = " << a / b;
+
+    return 0;
+}
